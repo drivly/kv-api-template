00d49eb08d056397523409941c7af117573df67f
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-07-27 02:37:38 -0500

    Added endpoints

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { Hono } from 'hono'
+import apis from './apis.json' assert {type: 'json'}
+
 const app = new Hono()
 
 app.get('/', (c) => c.json({hello: 'world'}))
+app.get('/api', (c) => c.json(apis))
+app.get('/api/:id', (c) => c.json(apis))
+app.get('/api/set/:key', (c) => c.json(apis))
 
 export default app
